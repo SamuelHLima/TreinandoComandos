ef9df30f021167c60f0e99d2f4032711ee6666ae
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-29 16:57:58 -0300

    :tada: 3º Exercicio concluido

---
+++ pratica-3.js
@@ -0,0 +1,16 @@
+function Conhecendo() {
+    
+    let nome =("Samuel");
+    let Email= ("samuelhenrique89@hotmail.com");
+    let profissão = ("AnalistaTecnico");
+    let Idade = (32);
+    let ehpalavra = typeof(nome) == "string";
+    let ehpalavra1 = typeof(Email) == "string";
+    let ehpalavra2 = typeof(profissão) == "string";
+    let ehpalavra3 = typeof(Idade) == "string";
+    console.log(ehpalavra,ehpalavra1, ehpalavra2, ehpalavra3)
+    
+    
+}
+
+Conhecendo()
