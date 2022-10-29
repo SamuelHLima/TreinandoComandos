35c2129b74244c2ce1b3d482184e4a6e7477dd1f
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-29 16:14:15 -0300

    :fire: refazendo os exercicios

---
+++ pratica-1.js
@@ -0,0 +1,12 @@
+function Conhecendo() {
+    
+    let nome =("Samuel");
+    let Email= ("samuelhenrique89@hotmail.com");
+    let profissão = ("AnalistaTecnico");
+    let Idade = (32);
+    console.log(nome,Email, profissão, Idade)
+    console.log(typeof(nome), typeof(Email), typeof( profissão), typeof( Idade ))
+    
+}
+
+Conhecendo()
