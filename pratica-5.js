d629902e39327d3e8464c44a9fc025adea5112e2
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-29 17:32:00 -0300

    :bug: 5º Exercicio concluido com sucesso

---
+++ pratica-5.js
@@ -0,0 +1,14 @@
+
+function Conhecendo() {
+    
+    let nome =("Samuel");
+    let Email= ("samuelhenrique89@hotmail.com");
+    let profissão = ("AnalistaTecnico");
+    let Idade = (32);
+    let Lugar = ("Bahamas");
+    console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${Lugar}. Confirma pra gente alguns dados? A sua idade é ${Idade}, você é ${profissão} e o seu email é ${Email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
+    
+    
+}
+
+Conhecendo()
