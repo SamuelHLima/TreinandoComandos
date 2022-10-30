c8579c9c9a31cb91ad6ad595a4a75584df9e4b60
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-30 19:51:50 -0300

    :bug: Exercicios 2 Pratica 7 feito

---
+++ Exercicios2/pratica-7.js
@@ -0,0 +1,19 @@
+
+function Idioma(X) {
+            if (X === 'es')
+            console.log("Hola Mundo!");
+        
+        if (X === 'pt')
+            console.log("Olá Mundo");
+        
+        if (X === 'en')
+            console.log("Hello Word");
+        
+        else if (!X)
+        console.log("Olá Mundo");
+    }
+    
+Idioma('pt');
+Idioma('en');
+Idioma('es');
+Idioma();
