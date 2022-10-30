93654b0e20a247345cb3e4372448ecfb5cdba94d
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-30 11:51:34 -0300

    exercicios 2 pratica 2 Feito :memo:

---
+++ Exercicios2/pratica-2.js
@@ -0,0 +1,21 @@
+
+function Comprador(X) {
+    console.log(`Olá, ${X}`)
+}
+Comprador('Tadeu');
+
+function mensagem(X,Y,Z) {
+    console.log(`Fizemos a entrega do produto ${X} no endereço ${Y} com sucesso!
+
+    O seu produto foi recebido por: ${Z}
+    
+    Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`)
+    
+}
+mensagem('Geladeira','Rua das Rendeiras,724, Floripa', 'Maria Rita')
+
+function empresa(X) {
+    console.log(`Anteciosamente,
+                    ${X}`)
+}
+empresa('MarioEletros@UR.com')
